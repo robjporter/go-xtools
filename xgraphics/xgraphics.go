79da67cede37f2ea62b145d1bff6a49d7589f716
Rob Porter <robporter79@outlook.com>
2018-07-07 09:15:14 +0100

    link fixes

--- xgraphics/xgraphics.go
+++ xgraphics/xgraphics.go
@@ -1,22 +1,21 @@
 package xgraphics
 
 import (
-	"./xborders"
-	"./xcolors"
-	"./xdisplay"
-	"./xicons"
-	"./xlines"
-	"./xprogress"
-	"./xtermo"
+	"github.com/robjporter/go-xtools/xgraphics/xborders"
+	"github.com/robjporter/go-xtools/xgraphics/xcolors"
+	"github.com/robjporter/go-xtools/xgraphics/xdisplay"
+	"github.com/robjporter/go-xtools/xgraphics/xicons"
+	"github.com/robjporter/go-xtools/xgraphics/xlines"
+	"github.com/robjporter/go-xtools/xgraphics/xprogress"
+	"github.com/robjporter/go-xtools/xgraphics/xtermo"
 )
 
-
 type Graphics struct {
-	Borders *xborders.Borders
-	Colors  *xcolors.Colors
-	Icons   *xicons.Icons
-	Lines   *xlines.Lines
-	Display *xdisplay.Display
+	Borders  *xborders.Borders
+	Colors   *xcolors.Colors
+	Icons    *xicons.Icons
+	Lines    *xlines.Lines
+	Display  *xdisplay.Display
 	Terminal *xtermo.Framebuffer
 }
 
@@ -24,7 +23,7 @@ func New() *Graphics {
 	xtermo.Init()
 	defer xtermo.Stop()
 	w, h, _ := xtermo.Size()
-	return &Graphics{Borders: xborders.New(), Colors: xcolors.New(), Icons: xicons.New(), Lines: xlines.New(), Display: xdisplay.New(),Terminal:xtermo.NewFramebuffer(w,h)}
+	return &Graphics{Borders: xborders.New(), Colors: xcolors.New(), Icons: xicons.New(), Lines: xlines.New(), Display: xdisplay.New(), Terminal: xtermo.NewFramebuffer(w, h)}
 }
 
 func NewProgress(title string) *xprogress.Xprogress {
